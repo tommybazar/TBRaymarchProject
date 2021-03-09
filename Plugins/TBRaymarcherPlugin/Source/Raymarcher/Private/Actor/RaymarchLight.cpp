1f4a195f89fb274079ec9380922b975c0f7d7e63
Tomas Bartipan <tombazar@seznam.cz>
2021-03-09 12:03:52 +0800

    license formatting stragglers

--- Plugins/TBRaymarcherPlugin/Source/Raymarcher/Private/Actor/RaymarchLight.cpp
+++ Plugins/TBRaymarcherPlugin/Source/Raymarcher/Private/Actor/RaymarchLight.cpp
@@ -1,6 +1,7 @@
 // Copyright 2021 Tomas Bartipan and Technical University of Munich.
 // Licensed under MIT license - See License.txt for details.
-// Special credits go to : Temaran (compute shader tutorial), TheHugeManatee (original concept, supervision) and Ryan Brucks (original raymarching code).
+// Special credits go to : Temaran (compute shader tutorial), TheHugeManatee (original concept, supervision) and Ryan Brucks
+// (original raymarching code).
 
 #include "Actor/RaymarchLight.h"
 
