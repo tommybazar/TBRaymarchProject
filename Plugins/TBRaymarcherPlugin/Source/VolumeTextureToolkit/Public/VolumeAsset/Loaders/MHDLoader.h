588bbb750f3d533b20e61e92ffb562d52af1396d
Tomas Bartipan <tombazar@seznam.cz>
2021-03-14 19:58:39 +0800

    cut out raymarcher plugin as a submodule

--- Plugins/TBRaymarcherPlugin/Source/VolumeTextureToolkit/Public/VolumeAsset/Loaders/MHDLoader.h
+++
@@ -1,4 +0,0 @@
-// Copyright 2021 Tomas Bartipan and Technical University of Munich.
-// Licensed under MIT license - See License.txt for details.
-// Special credits go to : Temaran (compute shader tutorial), TheHugeManatee (original concept, supervision) and Ryan Brucks
-// (original raymarching code).
